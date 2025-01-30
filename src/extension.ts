1b5171663f867283139218edc7eceb65185e6715
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2025-01-29 22:19:00 -0600

    ollama vscode extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,82 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import ollama from "ollama";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposaable = vscode.commands.registerCommand(
+    "mirage-ext.start",
+    () => {
+      const panel = vscode.window.createWebviewPanel(
+        "deepChat",
+        "Deepseek Chat",
+        vscode.ViewColumn.One,
+        { enableScripts: true }
+      );
+      panel.webview.html = getWebViewContent();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mirage-ext" is now active!');
+      panel.webview.onDidReceiveMessage(async (message: any) => {
+        if (message.command === "chat") {
+          const userPrompt = message.text;
+          let responseText = "";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mirage-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from mirage-ext!');
-	});
+          try {
+            const streamResposne = await ollama.chat({
+              model: "deepseek-r1:1.5b",
+              messages: [{ role: "user", content: userPrompt }],
+              stream: true,
+            });
 
-	context.subscriptions.push(disposable);
+            for await (const part of streamResposne) {
+              responseText += part.message.content;
+              console.log(responseText);
+              panel.webview.postMessage({
+                command: "chatResponse",
+                text: responseText,
+              });
+            }
+          } catch (err) {}
+        }
+      });
+    }
+  );
+
+  context.subscriptions.push(disposaable);
 }
 
-// This method is called when your extension is deactivated
+function getWebViewContent(): string {
+  return /*html*/ `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<style>
+			body {font-family: sans-serif; margin: 1rem; }
+			#prompt {width: 100%; box-sizing: border-box}
+			#response {border: 1px solid #ccc; margin-top: 1rem; padding: 0.5rem; min-height: 150px}
+		</style>
+	</head>
+	<body>
+		<h2>DeepSeek VS Code Extension</h2>
+		<textarea id="prompt" rows="3" placeholder="Ask something ..."></textarea><br />
+		<button id="askButton">Ask</button>
+		<div id="response"></div>
+		<script>
+			const vscode = acquireVsCodeApi();
+			
+			document.getElementById('askButton').addEventListener('click', () => {
+				const text = document.getElementById('prompt').value;
+				console.log(text)
+				vscode.postMessage({command: 'chat', text})
+			});
+
+			window.addEventListener('message', event => {
+				const {command, text} = event.data;
+				if (command === "chatResponse") {
+					document.getElementById("response").innerText = text;
+				}
+			})
+		</script>
+	</body>
+	`;
+}
+
 export function deactivate() {}
